bbd64b95518e4e8e79adace0c27bcf618d43e45c
Peusudocode <80451062+Peusudocode@users.noreply.github.com>
2022-01-20 01:13:20 +0800

    update4

---
+++ Addition_in_Column_Form/cmake-build-debug/CMakeFiles/Addition_in_Column_Form.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Addition_in_Column_Form.exe"
+  "Addition_in_Column_Form.exe.manifest"
+  "Addition_in_Column_Form.pdb"
+  "CMakeFiles/Addition_in_Column_Form.dir/main.c.obj"
+  "libAddition_in_Column_Form.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Addition_in_Column_Form.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
